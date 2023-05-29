85f5745c83bc90b3588e6a676998e8ea0a1fac17
eliseflaneuse <eliseflaneuse@gmail.com>
2023-05-29 15:31:37 -0300

    Exercicio de matrizes

--- src/vetoresMatrizes/Matriz_exe1.java
+++ src/vetoresMatrizes/Matriz_exe1.java
@@ -1,10 +1,46 @@
 package vetoresMatrizes;
 
+import java.util.Scanner;
+
 public class Matriz_exe1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int matrizInteiros[][] = new int[3][3];
+		int somaPrincipal = 0, somaSecundaria = 0;
+        
+		for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("Digite um valor para a posição [" + i + "][" + j + "]: ");
+                matrizInteiros[i][j] = leia.nextInt();
+            }
+        }
+		
+		// Imprimir todos os elementos da Diagonal Principal
+		System.out.println("\nElementos da Diagonal Principal:");
+		for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+				if (i == j) {
+					System.out.println(matrizInteiros[i][j]);
+					somaPrincipal += matrizInteiros[i][j];
+				}
+			}
+		}	
+		
+		// Imprimir todos os elementos da Diagonal Secundária
+		System.out.println("\nElementos da Diagonal Secundária:");
+		for (int l = matrizInteiros.length - 1; l >= 0; l--) {      
+			System.out.println(matrizInteiros[l][matrizInteiros.length - 1 - l]);
+			somaSecundaria += matrizInteiros[l][matrizInteiros.length - 1 - l];
+		}	
+		
+		//Soma das Diagonais
+		System.out.println("\nSoma dos Elementos da Diagonal Principal:");
+		System.out.println(somaPrincipal);
+		System.out.println("\nSoma dos Elementos da Diagonal Principal:");
+		System.out.println(somaSecundaria);
+		
 	}
-
 }
