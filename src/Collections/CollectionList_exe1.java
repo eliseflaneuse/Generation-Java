7fee090214249f23759c392b93d36a5b21a1f354
eliseflaneuse <eliseflaneuse@gmail.com>
2023-05-30 12:34:24 -0300

    Criei os arquivos para os exe de collections

---
+++ src/Collections/CollectionList_exe1.java
@@ -0,0 +1,15 @@
+package Collections;
+
+import java.util.Scanner;
+
+public class CollectionList_exe1 {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		
+
+	}
+
+}
---
+++ src/Collections/CollectionSet_exe2.java
@@ -0,0 +1,10 @@
+package Collections;
+
+public class CollectionSet_exe2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
