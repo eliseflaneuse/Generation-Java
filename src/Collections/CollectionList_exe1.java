eeee54c0fc0d8f9554aba26c629f44945ad221f0
eliseflaneuse <eliseflaneuse@gmail.com>
2023-05-30 13:02:27 -0300

    primeiro exercicio de list

--- src/Collections/CollectionList_exe1.java
+++ src/Collections/CollectionList_exe1.java
@@ -1,15 +1,36 @@
 package Collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class CollectionList_exe1 {
 
 	public static void main(String[] args) {
 		
+		ArrayList<String> cores = new ArrayList<String>();
+		
 		Scanner leia = new Scanner(System.in);
 		
-		
+        System.out.println("Digite 5 cores:");
 
+        for (int i = 0; i < 5; i++) {
+            String cor = leia.nextLine();
+            cores.add(cor);
+        }
+		
+        // Imprimir as cores
+        System.out.println("\nLista de todas as cores:");
+        for (String cor : cores) {
+        	System.out.println(cor);
+        }
+        
+        // Ordenar e imprimir as cores
+        Collections.sort(cores);
+        System.out.println("\nLista das cores ordenadas:");
+        for (String cor : cores) {
+        	System.out.println(cor);
+        }
 	}
 
 }
